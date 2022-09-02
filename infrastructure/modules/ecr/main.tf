bccd9c33dfbaa3e2bc5e792dd21dab88800deaa7
IzicTemi <iomolayo2541@gmail.com>
2022-09-02 21:55:17 +0000

    Minor changes to variables

--- infrastructure/modules/ecr/main.tf
+++ infrastructure/modules/ecr/main.tf
@@ -21,7 +21,7 @@ resource null_resource ecr_image {
    provisioner "local-exec" {
      command = <<EOF
              aws ecr get-login-password --region ${var.region} | docker login --username AWS --password-stdin ${var.account_id}.dkr.ecr.${var.region}.amazonaws.com
-             cd ../web-service-copy
+             cd ../web-service
              docker build -t ${aws_ecr_repository.repo.repository_url}:${var.ecr_image_tag} .
              docker push ${aws_ecr_repository.repo.repository_url}:${var.ecr_image_tag}
          EOF
--- infrastructure/vars/prod.tfvars
+++ infrastructure/vars/prod.tfvars
@@ -1,5 +1,5 @@
 model_bucket = "***REMOVED***s"
-lambda_function_local_path = "../web-service-copy/lambda_function.py"
-docker_image_local_path = "../web-service-copy/Dockerfile"
+lambda_function_local_path = "../web-service/lambda_function.py"
+docker_image_local_path = "../web-service/Dockerfile"
 ecr_repo_name = "prod-fake-news-detect"
 lambda_function_name = "prod-classify-lambda"

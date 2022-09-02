f78e7b4233edbb938f1676bdf3ab04184099d084
IzicTemi <iomolayo2541@gmail.com>
2022-09-02 21:54:24 +0000

    Integration test worksgit add integration-test/!

--- integration-test/run.sh
+++ integration-test/run.sh
@@ -4,7 +4,7 @@ if [[ -z "${GITHUB_ACTIONS}" ]]; then
   cd "$(dirname "$0")"
 fi
 
-if [ "${LOCAL_IMAGE_NAME}" == "" ]; then 
+if [ "${LOCAL_IMAGE_NAME}" == "" ]; then
     LOCAL_TAG=`date +"%Y-%m-%d-%H-%M"`
     export LOCAL_IMAGE_NAME="fake-news-classifier:${LOCAL_TAG}"
     echo "LOCAL_IMAGE_NAME is not set, building a new image with tag ${LOCAL_IMAGE_NAME}"

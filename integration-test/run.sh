c38bee6e31f0599bdb4210dfcae0de18690a6bab
IzicTemi <iomolayo2541@gmail.com>
2022-09-02 21:38:35 +0000

    Initial integration test

---
+++ integration-test/run.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+if [[ -z "${GITHUB_ACTIONS}" ]]; then
+  cd "$(dirname "$0")"
+fi
+
+if [ "${LOCAL_IMAGE_NAME}" == "" ]; then 
+    LOCAL_TAG=`date +"%Y-%m-%d-%H-%M"`
+    export LOCAL_IMAGE_NAME="fake-news-classifier:${LOCAL_TAG}"
+    echo "LOCAL_IMAGE_NAME is not set, building a new image with tag ${LOCAL_IMAGE_NAME}"
+    docker build -t ${LOCAL_IMAGE_NAME} ../web-service
+else
+    echo "no need to build image ${LOCAL_IMAGE_NAME}"
+fi
+
+docker-compose up -d
+
+sleep 5
+
+pipenv run python test.py
+
+ERROR_CODE=$?
+
+if [ ${ERROR_CODE} != 0 ]; then
+    docker-compose logs
+    docker-compose down
+    exit ${ERROR_CODE}
+fi
+
+docker-compose down

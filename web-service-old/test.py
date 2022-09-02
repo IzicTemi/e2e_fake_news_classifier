4973d3a3474258e1886cb5bce01cc22df4ede721
IzicTemi <iomolayo2541@gmail.com>
2022-09-02 21:40:27 +0000

    rearrange web services

---
+++ web-service-old/test.py
@@ -0,0 +1,11 @@
+import requests
+
+ride = {
+    "text": "I am a boy"
+}
+
+url = 'http://127.0.0.1:9696/classify'
+response = requests.post(url, json=ride)
+print(response.json())
+
+""" tag faker:v1"""

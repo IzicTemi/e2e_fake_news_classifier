9eed4d7bb176cfed0c7a7cad7a0a6de4ccea55c5
IzicTemi <iomolayo2541@gmail.com>
2022-09-03 02:45:57 +0000

    caught a minor bug if files not zip thanks to  testing

--- get_data.py
+++ get_data.py
@@ -1,10 +1,9 @@
 import os
+import zipfile
+from pathlib import Path
 
 import kaggle
 
-import zipfile
-
-from pathlib import Path
 
 KAGGLE_USERNAME = os.getenv("KAGGLE_USERNAME")
 KAGGLE_KEY = os.getenv("KAGGLE_KEY")
@@ -12,20 +11,20 @@ KAGGLE_KEY = os.getenv("KAGGLE_KEY")
 DATASET_KAGGLE_URL = 'clmentbisaillon/fake-and-real-news-***REMOVED***set'
 GLOVE_EMBEDDINGs_URL = 'icw123/glove-twitter'
 
-DATA_PATH = '***REMOVED***'
+DATA_PATH = os.getenv("DATA_PATH")
 
-def download_***REMOVED***set():
+Path(DATA_PATH).mkdir(parents=True, exist_ok=True)
+
+def download_***REMOVED***set(DATASET_KAGGLE_URL):
     '''
     Get ***REMOVED***set from Kaggle API
     '''
     print('Downloading Dataset')
     kaggle.api.authenticate()
-    kaggle.api.***REMOVED***set_download_files(
-        DATASET_KAGGLE_URL,
-        path=f'{DATA_PATH}'
-    )
+    kaggle.api.***REMOVED***set_download_files(DATASET_KAGGLE_URL, path=f'{DATA_PATH}')
 
-def download_glove():
+
+def download_glove(GLOVE_EMBEDDINGs_URL):
     '''
     Get glove embeddings from Kaggle API
     '''
@@ -34,17 +33,19 @@ def download_glove():
     kaggle.api.***REMOVED***set_download_file(
         GLOVE_EMBEDDINGs_URL,
         file_name='glove.twitter.27B.100d.txt',
-        path=f'{DATA_PATH}'
-        )
-    
+        path=f'{DATA_PATH}',
+    )
+
 
 def unzip():
     print('Unzipping...')
     dir_list = os.listdir(DATA_PATH)
     for file in dir_list:
-        with zipfile.ZipFile(f'{DATA_PATH}/{file}', 'r') as zip_ref:
-            print(f'Unzipping {file}')
-            zip_ref.extractall(DATA_PATH)
+        if file.endswith('.zip'):
+            with zipfile.ZipFile(f'{DATA_PATH}/{file}', 'r') as zip_ref:
+                print(f'Unzipping {file}')
+                zip_ref.extractall(DATA_PATH)
+
 
 def del_zip():
     print('Deleting zip files')
@@ -53,9 +54,10 @@ def del_zip():
         if file.endswith(".zip"):
             print(f'Deleting {file}')
             os.remove(Path(DATA_PATH).joinpath(file))
-    
+
+
 if __name__ == '__main__':
-    download_***REMOVED***set()
-    download_glove()
+    download_***REMOVED***set(DATASET_KAGGLE_URL)
+    download_glove(GLOVE_EMBEDDINGs_URL)
     unzip()
     del_zip()
--- tests/model_test.py
+++ tests/model_test.py
@@ -1,62 +1,68 @@
+import os
+import sys
 from pathlib import Path
 
-# import model
+import get_***REMOVED***
+
+# # lambda_function_path = str(Path.cwd().parent.absolute().joinpath('web_service'))
+
+# sys.path.insert(0, lambda_function_path)
+
+# from lambda_function import classify, lambda_handler
+
+# classify("ball")
+
+def test_get_***REMOVED***():
+    DATA_PATH = get_***REMOVED***.DATA_PATH
+    Path(DATA_PATH).mkdir(parents=True, exist_ok=True)
+    ***REMOVED***set = 'tarundalal/100-richest-people-in-world'
+    get_***REMOVED***.download_***REMOVED***set(***REMOVED***set)
+    assert '100-richest-people-in-world.zip' in os.listdir(DATA_PATH)
+    get_***REMOVED***.unzip()
+    assert 'TopRichestInWorld.csv' in os.listdir(DATA_PATH)
+    get_***REMOVED***.del_zip()
+    assert '100-richest-people-in-world.zip' not in os.listdir(DATA_PATH)
 
 
-def read_text(file):
-    test_directory = Path(__file__).parent
 
-    with open(test_directory / file, 'rt', encoding='utf-8') as f_in:
-        return f_in.read().strip()
+# class ModelMock:
+#     def __init__(self, value):
+#         self.value = value
 
-
-class ModelMock:
-    def __init__(self, value):
-        self.value = value
-
-    def predict(self, X):
-        n = len(X)
-        return [self.value] * n
+#     def classify(self, X):
+#         n = len(X)
+#         return [self.value] * n
 
 
-def test_predict():
-    model_mock = ModelMock(10.0)
-    model_service = model.ModelService(model_mock)
+# def test_predict():
+#     model_mock = ModelMock(10.0)
+#     model_service = model.ModelService(model_mock)
 
-    features = {
-        "PU_DO": "130_205",
-        "trip_distance": 3.66,
-    }
+#     features = {
+#         "PU_DO": "130_205",
+#         "trip_distance": 3.66,
+#     }
 
-    actual_prediction = model_service.predict(features)
-    expected_prediction = 10.0
+#     actual_prediction = model_service.predict(features)
+#     expected_prediction = 10.0
 
-    assert actual_prediction == expected_prediction
+#     assert actual_prediction == expected_prediction
 
 
-def test_lambda_handler():
-    model_mock = ModelMock(10.0)
-    model_version = 'Test123'
-    model_service = model.ModelService(model_mock, model_version)
+# def test_lambda_handler():
+#     model_mock = ModelMock(10.0)
+#     model_version = 'Test123'
+#     model_service = model.ModelService(model_mock, model_version)
 
-    base64_input = read_text('***REMOVED***.b64')
 
-    event = {
-        "text": "I am a boy"
-    }
+#     event = {
+#         "text": "I am a boy",
+#         }
 
-    actual_predictions = model_service.lambda_handler(event)
-    expected_predictions = {
-        'predictions': [
-            {
-                'model': 'ride_duration_prediction_model',
-                'version': model_version,
-                'prediction': {
-                    'ride_duration': 10.0,
-                    'ride_id': 256,
-                },
-            }
-        ]
-    }
+#     actual_predictions = model_service.lambda_handler(event)
+#     expected_predictions = {
+#         'text': event['text'],
+#         'class': 'boy'
+#     }
 
-    assert actual_predictions == expected_predictions
+#     assert actual_predictions == expected_predictions

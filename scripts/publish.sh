c088cf1d48d97c7c5b20704330ab319d4a6c391f
IzicTemi <iomolayo2541@gmail.com>
2022-09-08 22:54:04 +0000

    Remove hardcoded AWS_REGION and replaced with env

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,23 +4,23 @@ export ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com
 docker tag ${LOCAL_IMAGE_NAME} ${ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${LOCAL_IMAGE_NAME}
 docker push ${ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${LOCAL_IMAGE_NAME}
-STATE=$(aws lambda get-function --function-name ${LAMBDA_FUNCTION} --region "us-east-1" --query 'Configuration.LastUpdateStatus' --output text)
+STATE=$(aws lambda get-function --function-name ${LAMBDA_FUNCTION} --region ${AWS_DEFAULT_REGION} --query 'Configuration.LastUpdateStatus' --output text)
     while [[ "$STATE" == "InProgress" ]]
     do
         echo "sleep 5sec ...."
         sleep 5s
-        STATE=$(aws lambda get-function --function-name ${LAMBDA_FUNCTION} --region "us-east-1" --query 'Configuration.LastUpdateStatus' --output text)
+        STATE=$(aws lambda get-function --function-name ${LAMBDA_FUNCTION} --region ${AWS_DEFAULT_REGION} --query 'Configuration.LastUpdateStatus' --output text)
         echo $STATE
     done
 
 aws lambda update-function-code --function-name ${LAMBDA_FUNCTION} --image-uri ${ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${LOCAL_IMAGE_NAME}
 
-STATE=$(aws lambda get-function --function-name ${LAMBDA_FUNCTION} --region "us-east-1" --query 'Configuration.LastUpdateStatus' --output text)
+STATE=$(aws lambda get-function --function-name ${LAMBDA_FUNCTION} --region ${AWS_DEFAULT_REGION} --query 'Configuration.LastUpdateStatus' --output text)
     while [[ "$STATE" == "InProgress" ]]
     do
         echo "sleep 5sec ...."
         sleep 5s
-        STATE=$(aws lambda get-function --function-name ${LAMBDA_FUNCTION} --region "us-east-1" --query 'Configuration.LastUpdateStatus' --output text)
+        STATE=$(aws lambda get-function --function-name ${LAMBDA_FUNCTION} --region ${AWS_DEFAULT_REGION} --query 'Configuration.LastUpdateStatus' --output text)
         echo $STATE
     done
 
